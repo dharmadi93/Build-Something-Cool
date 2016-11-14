9530c8e1beed2eb496ea4df10a51c8bc75cd7150
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-15 00:47:28 +0700

    create controller user crud sctructure

---
+++ server/controllers/controller.api.user.js
@@ -0,0 +1,23 @@
+const Employee = require('../models/employee')
+
+module.exports = {
+    getAllEmployee: (req, res) => {
+
+    },
+
+    getEmployeeById: (req, res) => {
+
+    },
+
+    createEmployee: (req, res) => {
+
+    },
+
+    deleteEmployee: (req, res) => {
+
+    },
+
+    updateEmployee: (req, res) => {
+
+    }
+}
