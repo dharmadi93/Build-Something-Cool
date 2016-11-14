ba6079cacf4cacf2880fa45cc67cc2eabf906674
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-15 00:31:02 +0700

    add column item id

--- server/models/transactiondetail.js
+++ server/models/transactiondetail.js
@@ -10,6 +10,7 @@ module.exports = function(sequelize, DataTypes) {
       associate: function(models) {
         // associations can be defined here
         TransactionDetail.belongsTo(models.Transaction)
+        TransactionDetail.belongsTo(models.Item)
       }
     }
   });
