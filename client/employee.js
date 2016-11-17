f144590bb11610d3211ba2347ce430f5efceced4
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-17 21:14:20 +0700

    update employee in client works

--- client/employee.js
+++ client/employee.js
@@ -16,6 +16,7 @@ function getAllEmployeeList() {
                 <tr id="employee${data[i].id}">
                     <td>${data[i].name}</td>
                     <td>${data[i].username}</td>
+                    <td>${data[i].password}</td>
                     <td>${data[i].email}</td>    
                     <td>${data[i].role}</td>    
                     <td>
@@ -31,11 +32,12 @@ function getAllEmployeeList() {
     })
 }
 
-function replaceEmployee(data) {
+function appendEmployee(data) {
     let html = `
                 <tr id="employee${data.id}">
                     <td>${data.name}</td>
                     <td>${data.username}</td>
+                    <td>${data.password}</td>
                     <td>${data.email}</td>    
                     <td>${data.role}</td>    
                     <td>
@@ -45,6 +47,23 @@ function replaceEmployee(data) {
                 </tr>
 `
     $(`#employeeList`).append(html)
+}
+
+function replaceEmployee(data) {
+    let html = `
+                <tr id="employee${data.id}">
+                    <td>${data.name}</td>
+                    <td>${data.username}</td>
+                    <td>${data.password}</td>
+                    <td>${data.email}</td>    
+                    <td>${data.role}</td>    
+                    <td>
+                        <a name="buttonEdit" class="btn btn-warning" data-id="${data.id}">Edit</a>
+                        <a name="buttonDelete" class="btn btn-danger" data-id="${data.id}">Delete</a>
+                    </td>    
+                </tr>
+`
+    $(`#employee${data.id}`).replaceWith(html)
 }
 
 $(document).on('click', 'a[name="buttonDelete"]', function () {
@@ -105,5 +124,68 @@ function updateViewAfterCreateEmployee(data) {
     $('input[name="username"]').val('')
     $('input[name="password"]').val('')
     $('input[name="email"]').val('')
+    appendEmployee(data)
+}
+
+$(document).on('click', 'a[name="buttonEdit"]', function () {
+    let employeeId = this.getAttribute('data-id')
+    getEmployeForEdit(employeeId)
+})
+
+function getEmployeForEdit(employeeId) {
+    $.ajax({
+        url: `${URL_EMPLOYEE}/${employeeId}`,
+        method: 'get',
+        success: function (data) {
+            $('#editEmployeeModal').modal('show')
+            $('input[name="nameEdit"]').val(data.name)
+            $('input[name="usernameEdit"]').val(data.username)
+            $('input[name="passwordEdit"]').val(data.password)
+            $('input[name="emailEdit"]').val(data.email)
+
+            let temp = $("input[name='id']").val()
+            if ( typeof temp != "undefined") {
+                $("input[name='id']").replaceWith(`<input type='hidden' class='form-control' name='id' value='${data.id}'>`)
+            }
+            else {
+                $("#formEmployeeUpdate").append(`<input type='hidden' class='form-control' name='id' value='${data.id}'>`)
+            }
+        }
+    })
+}
+
+$(document).on('click', 'button[name="buttonUpdateEmployee"]', function (e) {
+    e.preventDefault()
+    let id = $('input[name="id"]').val()
+    let name = $('input[name="nameEdit"]').val()
+    let username = $('input[name="usernameEdit"]').val()
+    let password = $('input[name="passwordEdit"]').val()
+    let email = $('input[name="emailEdit"]').val()
+    updateEmployee(id, name, username, password, email)
+})
+
+function updateEmployee(id, name, username, password, email) {
+    $.ajax({
+        url: `${URL_EMPLOYEE}/${id}`,
+        method: 'put',
+        contentType: `${CONTENT_TYPE}`,
+        data: {
+            name: name,
+            username: username,
+            password: password,
+            email: email
+        },
+        success: function (data) {
+            updateViewAfterUpdateEmployee(data)
+        }
+    })
+}
+
+function updateViewAfterUpdateEmployee(data) {
+    $('#editEmployeeModal').modal('hide')
+    $('input[name="nameEdit"]').val('')
+    $('input[name="usernameEdit"]').val('')
+    $('input[name="passwordEdit"]').val('')
+    $('input[name="emailEdit"]').val('')
     replaceEmployee(data)
 }
